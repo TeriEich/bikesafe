e4eae3ddaf5776f5dc5888e2ef25b2b6c6f4f941
Umair Abdul Qadir <umair.abdulq@gmail.com>
2019-01-28 19:28:38 +0000

    Iniital commit.

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AccidentSchema = new Schema(
+	{
+	id: Number,
+	location: {
+		type: {
+			type: String,
+			enum: ['Point'],
+			required: true
+		},
+		coordinates: {
+			type: [Number],
+			required: true
+		}
+	},
+	date: Date,
+	year: Number,
+	time: Number,
+	hour: Number, 
+	street1: String,
+	street2: String,
+	visibility: String,
+	light: String,
+	roadConditions: String,
+	injuryType: String,
+	neighbourhood: String
+	});
+
+const TheftSchema = new Schema(
+	{
+	id: Number,
+	location: {
+		type: {
+			type: String,
+			enum: ['Point'],
+			required: true
+		},
+		coordinates: {
+			type: [Number],
+			required: true
+		}
+	},
+	occurrenceYear: Number,
+	occurrenceMonth: Number,
+	occurrenceDay: Number,
+	occurrenceTime: String,
+	bikeMake: String,
+	bikeModel: String,
+	neighbourhood: String
+	});
+
+
+var Accident = mongoose.model('Accident', AccidentSchema);
+var Theft = mongoose.model('Theft', TheftSchema);
+var schemas = {'Accident': Accident, 'Theft': Theft}
+module.exports = schemas
