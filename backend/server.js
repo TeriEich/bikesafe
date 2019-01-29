63f8f6a1e278fe495d68f83b1192659610445d56
Umair Abdul Qadir <umair.abdulq@gmail.com>
2019-01-29 17:16:30 +0000

    Creating seed data, schemas and seeding MLab databases.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const Data = require("./data");
+const schemas = require("./schemas");
+const Accident = schemas.Accident;
+const Theft = schemas.Theft;
 
 const API_PORT = 3001;
 const app = express();
@@ -37,53 +39,53 @@ app.use(logger("dev"));
 
 // this is our get method
 // this method fetches all available data in our database
-router.get("/getData", (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
-});
+// router.get("/getData", (req, res) => {
+//   Data.find((err, data) => {
+//     if (err) return res.json({ success: false, error: err });
+//     return res.json({ success: true, data: data });
+//   });
+// });
 
-// this is our update method
-// this method overwrites existing data in our database
-router.post("/updateData", (req, res) => {
-  const { id, update } = req.body;
-  Data.findOneAndUpdate(id, update, err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
+// // this is our update method
+// // this method overwrites existing data in our database
+// router.post("/updateData", (req, res) => {
+//   const { id, update } = req.body;
+//   Data.findOneAndUpdate(id, update, err => {
+//     if (err) return res.json({ success: false, error: err });
+//     return res.json({ success: true });
+//   });
+// });
 
-// this is our delete method
-// this method removes existing data in our database
-router.delete("/deleteData", (req, res) => {
-  const { id } = req.body;
-  Data.findOneAndDelete(id, err => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
+// // this is our delete method
+// // this method removes existing data in our database
+// router.delete("/deleteData", (req, res) => {
+//   const { id } = req.body;
+//   Data.findOneAndDelete(id, err => {
+//     if (err) return res.send(err);
+//     return res.json({ success: true });
+//   });
+// });
 
-// this is our create methid
-// this method adds new data in our database
-router.post("/putData", (req, res) => {
-  let data = new Data();
+// // this is our create methid
+// // this method adds new data in our database
+// router.post("/putData", (req, res) => {
+//   let data = new Data();
 
-  const { id, message } = req.body;
+//   const { id, message } = req.body;
 
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: "INVALID INPUTS"
-    });
-  }
-  data.message = message;
-  data.id = id;
-  data.save(err => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
+//   if ((!id && id !== 0) || !message) {
+//     return res.json({
+//       success: false,
+//       error: "INVALID INPUTS"
+//     });
+//   }
+//   data.message = message;
+//   data.id = id;
+//   data.save(err => {
+//     if (err) return res.json({ success: false, error: err });
+//     return res.json({ success: true });
+//   });
+// });
 
 // append /api for our http requests
 app.use("/api", router);
