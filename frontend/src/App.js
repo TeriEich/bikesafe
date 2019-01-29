612b90c8e57f388ee70069d2ee3ce96938667c74
Umair Abdul Qadir <umair.abdulq@gmail.com>
2019-01-29 23:38:16 +0000

    Installing reactstrap and styling the navigation bar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from './NavBar';
 import MainMap from './Map'
 
 class App extends Component {
@@ -41,55 +42,6 @@ class App extends Component {
       </footer>
     </div>
   );
-  }
-}
-
-class NavBar extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isHidden: true
-    };
-  }
-
-  _openForm = () => this.setState({ isHidden: !this.state.isHidden });
-
-  render() {
-    return (
-      <nav className="nav-bar">
-        <h2 className="nav-brand">bikesafeTO</h2>
-        <button className="report-form-btn" type="button" onClick={() => this._openForm()}>
-          Submit a Report
-        </button>
-        { !this.state.isHidden &&
-        <div id="report-form" placement="right" container={this}>
-          <form>
-            <div className="container-incident-type">
-              <p>Are you reporting a theft or an accident?</p>
-              <br/>
-              <input type="radio" id="radio-theft" name="radioGroup" inline="true" />
-              <label htmlFor="radio-theft">Theft</label>
-              {' '}
-              <input type="radio" id="radio-accident" name="radioGroup" inline="true" />
-              <label htmlFor="radio-accident">Traffic Accident</label>
-              {' '}
-            </div>
-            <div id="form-date-time">
-              <label htmlFor="form-date-time">Select the date and approximate time of the incident</label>
-              <br/>
-              <input type="datetime-local" name="incident-datetime" />
-            </div>
-            <div className="form-submit">
-              <button type="submit">Submit Report</button>
-              <p>The submission of this form *DOES NOT* send any information to the Toronto Police.</p>
-              <p>To file an official police report, please contact your local precinct for assistance.</p>
-            </div>
-          </form>
-        </div> }
-      </nav>
-      );
-
   }
 }
 
